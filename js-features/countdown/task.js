e7551251b16affe5ee98bc3e83588917222d3c56
Pavel Novikov <pchela57@inbox.ru>
2019-12-18 08:14:56 +0400

    homework finished

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,9 @@
-console.log
+const addText = function () {
+  const output = document.getElementById("timer");
+  output.textContent -= 1;
+  if (output.textContent <= 0) {
+      alert('Вы победили в конкурсе');
+      clearInterval(inervalID);
+  }
+}
+const inervalID = setInterval(addText, 1000); 
